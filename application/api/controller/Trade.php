298750a2c34443acfc97c6d49b364adb40eb69c7
Li Hui Feng <xx90ff@163.com>
2021-03-02 13:10:31 +0800

    test111

--- application/api/controller/Trade.php
+++ application/api/controller/Trade.php
@@ -19,6 +19,8 @@ class Trade extends Api
      */
     public function notice()
     {
+        $json = file_get_contents('php://input');
+        print_r($json);die;
         // 接收到的订购消息密文
         $message = 'zanO3UZzEA5Fm6qKsCq6rJ70VoKdiqXDfgdqGvzOfIZ%2FVNB%2FUHuG7%2F6TxdL6NQIVkO8CFm20whTivoPj4%2B6nXLAoe9J%2BZc42nRAbkTg5GruMl8RohxSDS0%2F99FwGtLW9TmMbxs554ZWVaRaiB5KaHF%2FNTzuLHyEtrLB2xu8Y%2BAnMN%2FVVVO9PPgO8o1BSAuvJdNXa1%2ButpG%2BRSSSMbxXrvCkRC34X7kCK1z5Xg51r%2Fym8nxrrSFn2c4R3rMRxKQAMmzRfGBkcQ9XayS31oT5DNy0h5pWzP8W5pe9naUguCiPIIAqGmBo8etlIn1Y1FHAU';
 
