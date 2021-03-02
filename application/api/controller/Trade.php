981a91092c48152424a682dc5da933ed35a909b2
Li Hui Feng <xx90ff@163.com>
2021-03-02 13:14:06 +0800

    test222

--- application/api/controller/Trade.php
+++ application/api/controller/Trade.php
@@ -19,17 +19,17 @@ class Trade extends Api
      */
     public function notice()
     {
-        $json = file_get_contents('php://input');
-        print_r($json);die;
+        $json = file_get_contents('php://input');print_r($json);
+        file_put_contents('youzan.txt',$json);
         // 接收到的订购消息密文
-        $message = 'zanO3UZzEA5Fm6qKsCq6rJ70VoKdiqXDfgdqGvzOfIZ%2FVNB%2FUHuG7%2F6TxdL6NQIVkO8CFm20whTivoPj4%2B6nXLAoe9J%2BZc42nRAbkTg5GruMl8RohxSDS0%2F99FwGtLW9TmMbxs554ZWVaRaiB5KaHF%2FNTzuLHyEtrLB2xu8Y%2BAnMN%2FVVVO9PPgO8o1BSAuvJdNXa1%2ButpG%2BRSSSMbxXrvCkRC34X7kCK1z5Xg51r%2Fym8nxrrSFn2c4R3rMRxKQAMmzRfGBkcQ9XayS31oT5DNy0h5pWzP8W5pe9naUguCiPIIAqGmBo8etlIn1Y1FHAU';
-
-        // 应用的clientSecret
-        $youzan = config('site.youzan');
-        $youzan['client_secret'] = 'def00000b1228e0f6ba8ef96be9';
-        //解密
-        $res = CryptoHelper::decrypt($message, $youzan['client_secret']);
-        print_r($res);
+//        $message = 'zanO3UZzEA5Fm6qKsCq6rJ70VoKdiqXDfgdqGvzOfIZ%2FVNB%2FUHuG7%2F6TxdL6NQIVkO8CFm20whTivoPj4%2B6nXLAoe9J%2BZc42nRAbkTg5GruMl8RohxSDS0%2F99FwGtLW9TmMbxs554ZWVaRaiB5KaHF%2FNTzuLHyEtrLB2xu8Y%2BAnMN%2FVVVO9PPgO8o1BSAuvJdNXa1%2ButpG%2BRSSSMbxXrvCkRC34X7kCK1z5Xg51r%2Fym8nxrrSFn2c4R3rMRxKQAMmzRfGBkcQ9XayS31oT5DNy0h5pWzP8W5pe9naUguCiPIIAqGmBo8etlIn1Y1FHAU';
+//
+//        // 应用的clientSecret
+//        $youzan = config('site.youzan');
+//        $youzan['client_secret'] = 'def00000b1228e0f6ba8ef96be9';
+//        //解密
+//        $res = CryptoHelper::decrypt($message, $youzan['client_secret']);
+//        print_r($res);
         // {"appId":110,"buyerId":123456,"buyerPhone":"13800138000","env":"PROD","kdtId":160,"orderNo":"E201905060000001","payTime":1557138032000,"price":42,"skuIntervalText":"7","skuVersionText":"试用版","status":20,"type":"APP_SUBSCRIBE"}
 
         //$this->success('请求成功');
